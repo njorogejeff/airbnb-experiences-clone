5ecdd3587a81a1d8d1c8df853f4ebbdbdf63adee
Njoroge Jeff <njorogejeff254@gmail.com>
2024-05-10 17:23:57 +0300

    added a card component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/Card"
 import './App.css'
 
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
+      {/* <Hero /> */}
+      <Card />
     </>
   )
 }
