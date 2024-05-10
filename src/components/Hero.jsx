9c7168c07c80e9a777b7e4578e63646181ecdfca
Njoroge Jeff <njorogejeff254@gmail.com>
2024-05-10 17:26:07 +0300

    refactor hero component for improved readability and responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,10 @@ import photoGrid from '../assets/photo-grid.svg'
 
 export default function Hero() {
     return (
-        <section className='px-8 py-6' style={{minHeight: 'calc(100vh - 4.5rem)'}}>
-            <div className='grid place-items-center mb-8'>
-                <img src={photoGrid} alt="Various online experiences" className='w-3/4' />
-            </div>
-            <h1 className='text-4xl font-semibold mb-4'>Online Experiences</h1>
-            <p className='text-base font-light text-[#222] leading-[1.1rem]'>
-                Join unique interactive activites led by one-of-a-kind hosts-all without leaving home.
-            </p>
+        <section className='px-9 py-6'>
+            <img src={photoGrid} alt="Airbnb Online Experiences" className='mb-8 w-full max-h-[30rem]' />
+            <h1 className='text-4xl font-semibold leading-10 mb-4'>Online Experiences</h1>
+            <p className='text-[#222] text-base font-light leading-4 max-w-80 md:max-w-full'>Join unique interactive activites led by one-of-a-kind hosts-all without leaving home.</p>
         </section>
     )
 }
