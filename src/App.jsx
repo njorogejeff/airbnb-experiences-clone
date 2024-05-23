bdaa27a084dd04fd2aa19377460ab7a369b18ffb
Njoroge Jeff <njorogejeff254@gmail.com>
2024-05-23 21:02:25 +0300

    feat: Implement lazy loading for images in App.jsx
    The code changes in App.jsx implement lazy loading for images by importing the `data` module and rendering a list of `Card` components based on the data. This improves the page load performance by loading images only when they are needed.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,27 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
 import './App.css'
+import data from './data'
+
+console.log(data);
 
 function App() {
+  const cards = data.map(card => {
+    return (
+      <Card
+        key={card.id}
+        item={card}
+      />
+    )
+  })
+
   return (
     <>
       <Navbar />
-      {/* <Hero /> */}
-      <Card />
+      <Hero />
+      <section className='px-9 py-6 flex justify-start items-center gap-x-5'>
+        {cards}
+      </section>
     </>
   )
 }
